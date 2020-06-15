58ce7204549869755e33e66f1d70a4a601679e06
lucianoleme <lucianogonzagaleme@gmail.com>
2020-06-15 01:45:28 -0300

    CommandContext etc.

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -7,15 +7,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CommandAPI.Models;
 
 namespace CommandAPI
 {
   public class Startup
   {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration) => Configuration = configuration;
+
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+        (Configuration.GetConnectionString("PostgreSqlConnection")));
       services.AddControllers();
     }
 
@@ -31,11 +38,11 @@ namespace CommandAPI
 
       app.UseEndpoints(endpoints =>
       {
-              // endpoints.MapGet("/", async context =>
-              // {
-              //     await context.Response.WriteAsync("Hello World!");
-              // });
-              endpoints.MapControllers();
+        // endpoints.MapGet("/", async context =>
+        // {
+        //     await context.Response.WriteAsync("Hello World!");
+        // });
+        endpoints.MapControllers();
       });
     }
   }
